9d61f8fe3255653186afd5dc9a103243056b1786
IngaKirsona <ingas@inbox.lv>
2020-09-19 09:21:40 +0300

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Inga Kirsona on 19/09/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
