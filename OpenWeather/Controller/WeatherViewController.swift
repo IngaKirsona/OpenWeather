13796f48ad53dbaa3ae7c7b74ddd46de55ddc279
IngaKirsona <ingas@inbox.lv>
2020-09-19 14:17:57 +0300

    1:28:58

--- OpenWeather/Controller/WeatherViewController.swift
+++ OpenWeather/Controller/WeatherViewController.swift
@@ -23,16 +23,26 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         locationManager.delegate = self
 //-------> how accurate location will be
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
-    
+    //MARK: - Location Manager Delegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        <#code#>
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0{
+            locationManager.stopUpdatingLocation()
+            
+            print ("Longitude: \(location.coordinate.longitude), latitude: \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String (location.coordinate.longitude)
+            
+            let params: [String: String] = ["lat": latitude, "long": longitude, "appid": weatherDataModel.apiId]
+            getWeatherData(url: weatherDataModel.apiUrl, parameters: params)
+        }
     }
     
 //-------> if something went wrong with requesting weather, this will appear
@@ -41,5 +51,17 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate{
         cityLabel.text = "Weather Unavailable ⛔️"
     }
 
+    //MARK: Networking
+    func getWeatherData(url: String, parameters: [String: String]){
+        
+        AF.request(url, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.value != nil {
+                print("Got weather data")
+                let weatherJSON:JSON = JSON(response.value!)
+                print("weatherJSON: ", weatherJSON)
+            }
+        }
+        
+    }
 }
 
