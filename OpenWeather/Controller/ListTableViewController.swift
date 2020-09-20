fa23d8b5e27a00b3cff35d05ab0db3354edc9eb0
IngaKirsona <ingas@inbox.lv>
2020-09-20 16:48:14 +0300

    Hometask finished!

--- OpenWeather/Controller/ListTableViewController.swift
+++ OpenWeather/Controller/ListTableViewController.swift
@@ -16,16 +16,31 @@ class ListTableViewController: UITableViewController {
         "https://github.com/IngaKirsona/UIElementsLayoutHomework",
         "https://github.com/IngaKirsona/WeeklyDayFinder",
         "https://github.com/IngaKirsona/ConverterTemperature",
-        "https://github.com/IngaKirsona/ConverterTemperature",
         "https://github.com/IngaKirsona/LifeCycle",
         "https://github.com/IngaKirsona/MovieList",
         "https://github.com/IngaKirsona/LoginApp",
         "https://github.com/IngaKirsona/ShoppingList",
         "https://github.com/IngaKirsona/PoekmonCards",
         "https://github.com/IngaKirsona/WebRecipes",
-        "https://github.com/IngaKirsona/RestCountries"
+        "https://github.com/IngaKirsona/RestCountries",
+        "https://github.com/IngaKirsona/tasks"
     ]
     
+    var githubnames = [
+        "Dark Mode App",
+        "Trafic Light",
+        "UI Elements",
+        "Weekly Day Finder",
+        "Temperature Converter",
+        "Life Cycle",
+        "Movie List",
+        "Login App",
+        "Shopping List",
+        "Pokemon Cards",
+        "Web Recipes",
+        "Rest Countries",
+        "Homeworks in Xcode Playground"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,8 +51,9 @@ class ListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "webCell", for: indexPath)
-        cell.textLabel?.text = githubList[indexPath.row]
+        cell.textLabel?.text = githubnames[indexPath.row]
         cell.textLabel?.numberOfLines = 0
+        cell.selectionStyle = .none
         return cell
     }
     //-------> when clicked on row, navigate to webViewController
@@ -48,5 +64,10 @@ class ListTableViewController: UITableViewController {
         vc.passedValue = githubList[indexPath.row]
         self.present(vc, animated: true)
     }
-    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        UIView.animate(withDuration: 0.7){
+            cell.transform = CGAffineTransform.identity
+        }
+    }
 }
